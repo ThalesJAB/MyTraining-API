8e6872962abe7ff787964a3bba4794007630d4e1
Thales Jose <thalesjoseaguiar@gmail.com>
2023-05-20 16:15:57 -0300

    added query in WorkoutPlanRepository

--- src/main/java/br/com/mytraining/repositories/WorkoutPlanRepository.java
+++ src/main/java/br/com/mytraining/repositories/WorkoutPlanRepository.java
@@ -1,9 +1,16 @@
 package br.com.mytraining.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import br.com.mytraining.entities.WorkoutPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlan, Long>{
+
+    @Query("SELECT w FROM WorkoutPlan w WHERE w.person.id = :idPerson")
+    List<WorkoutPlan> findAllWorkoutPlanByIdPerson(@Param(value="idPerson") Long idPerson);
+
 
 }
