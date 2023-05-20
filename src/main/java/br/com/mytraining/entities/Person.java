e275aef1cde27a9e7be7bf19851a4a1f6b06f975
Thales Jose <thalesjoseaguiar@gmail.com>
2023-05-20 16:25:38 -0300

    java version change to 17

--- src/main/java/br/com/mytraining/entities/Person.java
+++ src/main/java/br/com/mytraining/entities/Person.java
@@ -1,110 +1,106 @@
 package br.com.mytraining.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-
 @Entity
 public class Person implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private Integer age;
-	private Double weight;
-	private Double height;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private Double weight;
+    private Double height;
 
-	@OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
-	private List<WorkoutPlan> workoutPlans = new ArrayList<>();
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
+    private List<WorkoutPlan> workoutPlans = new ArrayList<>();
 
-	public Person() {
+    public Person() {
 
-	}
+    }
 
-	public Person(Long id, String name, Integer age, Double weight, Double height, List<WorkoutPlan> workoutPlans) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-		this.weight = weight;
-		this.height = height;
-		this.workoutPlans = workoutPlans;
-	}
+    public Person(Long id, String name, Integer age, Double weight, Double height, List<WorkoutPlan> workoutPlans) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+        this.workoutPlans = workoutPlans;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Integer getAge() {
-		return age;
-	}
+    public Integer getAge() {
+        return age;
+    }
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	public Double getWeight() {
-		return weight;
-	}
+    public Double getWeight() {
+        return weight;
+    }
 
-	public void setWeight(Double weight) {
-		this.weight = weight;
-	}
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
 
-	public Double getHeight() {
-		return height;
-	}
+    public Double getHeight() {
+        return height;
+    }
 
-	public void setHeight(Double height) {
-		this.height = height;
-	}
+    public void setHeight(Double height) {
+        this.height = height;
+    }
 
-	public List<WorkoutPlan> getWorkoutPlans() {
-		return workoutPlans;
-	}
+    public List<WorkoutPlan> getWorkoutPlans() {
+        return workoutPlans;
+    }
 
-	public void setWorkoutPlans(List<WorkoutPlan> workoutPlans) {
-		this.workoutPlans = workoutPlans;
-	}
+    public void addWorkoutPlans(WorkoutPlan plan) {
+        this.workoutPlans.add(plan);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    public void setWorkoutPlans(List<WorkoutPlan> workoutPlans) {
+        this.workoutPlans = workoutPlans;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		return Objects.equals(id, other.id);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Person other = (Person) obj;
+        return Objects.equals(id, other.id);
+    }
 
 }
