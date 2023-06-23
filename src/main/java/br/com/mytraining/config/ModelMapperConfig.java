3a7a54f2d36a73b3920402f49ea4c52f8fd7e016
Thales Jose <thalesjoseaguiar@gmail.com>
2023-06-23 01:06:16 -0300

    creation of ModelMapperConfig

---
+++ src/main/java/br/com/mytraining/config/ModelMapperConfig.java
@@ -0,0 +1,31 @@
+package br.com.mytraining.config;
+
+
+import br.com.mytraining.entities.enums.ProfileType;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+
+	@Bean
+	public ModelMapper mapper() {
+
+		ModelMapper modelMapper = new ModelMapper();;
+
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+		modelMapper.createTypeMap(ProfileType.class, Integer.class)
+				.setConverter(context -> {
+					ProfileType profileType = context.getSource();
+					return profileType.getCode();
+				});
+		
+		return modelMapper;
+		
+	}
+
+}
