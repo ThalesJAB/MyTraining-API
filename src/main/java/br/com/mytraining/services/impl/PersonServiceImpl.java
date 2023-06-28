f70d1fb15b58636f56f2451454be2e1c225a1add
Thales Jose <thalesjoseaguiar@gmail.com>
2023-06-28 02:34:32 -0300

    update on PersonServiceImpl(password encoder)

--- src/main/java/br/com/mytraining/services/impl/PersonServiceImpl.java
+++ src/main/java/br/com/mytraining/services/impl/PersonServiceImpl.java
@@ -1,21 +1,24 @@
 package br.com.mytraining.services.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.mytraining.entities.Person;
 import br.com.mytraining.repositories.PersonRepository;
 import br.com.mytraining.services.PersonService;
+import br.com.mytraining.services.exceptions.DataIntegrityViolationException;
 import br.com.mytraining.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonServiceImpl implements PersonService {
 
 	@Autowired
 	private PersonRepository repository;
+
+	private BCryptPasswordEncoder encoder;
 
 	@Override
 	public List<Person> findAll() {
@@ -33,22 +36,32 @@ public class PersonServiceImpl implements PersonService {
 
 	@Override
 	public Person create(Person obj) {
+		validateEmail(obj);
+
+		obj.setPassword(encoder.encode(obj.getPassword()));
+
 		return repository.save(obj);
 	}
+
 
 	@Override
 	public Person update(Long id, Person obj) {
 
 		Person entity = findById(id);
 
+		validateEmail(obj);
+		entity.setEmail(obj.getEmail());
+		if(!obj.getPassword().equals(entity.getPassword())) {
+			entity.setPassword(encoder.encode(obj.getPassword()));
+		}
+		entity.setPassword(obj.getPassword());
 		entity.setAge(obj.getAge());
 		entity.setHeight(obj.getHeight());
 		entity.setName(obj.getName());
 		entity.setWeight(obj.getWeight());
+		obj.getWorkoutPlans().forEach(entity::addWorkoutPlans);
+		obj.getProfiles().forEach(entity::addProfile);
 
-		if (obj.getWorkoutPlans() != null) {
-			entity.setWorkoutPlans(obj.getWorkoutPlans());
-		}
 
 		return repository.save(entity);
 	}
@@ -56,6 +69,16 @@ public class PersonServiceImpl implements PersonService {
 	@Override
 	public void delete(Long id) {
 		repository.deleteById(id);
+
+	}
+
+
+	private void validateEmail(Person obj) {
+		Optional<Person> personDB = repository.findByEmail(obj.getEmail());
+
+		if(personDB.isPresent() && !personDB.get().getId().equals(obj.getId())){
+			throw new DataIntegrityViolationException("E-mail já cadastrado!");
+		}
 
 	}
 
